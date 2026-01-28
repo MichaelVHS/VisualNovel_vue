3d04d0a0d39302d4b05666c04a2fdef4f4892326
MichaelVHS <101734936+MichaelVHS@users.noreply.github.com>
2026-01-28 21:26:57 +0300

    Add files via upload

---
+++ src/components/Saves.vue
@@ -0,0 +1,55 @@
+<template>
+  <body>
+  <div class="containers">
+    <div class="container" v-for="i in 8" :key="i"></div>
+  </div>
+  <router-link to="/" class="saves_text">Назад</router-link>
+  </body>
+</template>
+
+<style scoped>
+*{
+  box-sizing: border-box;
+}
+
+html{
+  height: 100%;
+  width: 100%;
+}
+
+body {
+  margin: 0;
+  display: flex;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.containers {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 10px;
+  background-color: darkolivegreen;
+  padding: 40px;
+}
+.container {
+  width: 300px;
+  height: 200px;
+  background-color: black;
+}
+.saves_text {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  margin: 20px;
+  font-size: 25px;
+  color: white;
+}
+</style>
+<script setup lang="ts">
+</script>
---
+++ src/components/Settings.vue
@@ -0,0 +1,35 @@
+<template>
+  <body>
+  <div class="novel">
+    <router-link to="/" class="game_text">Назад</router-link>
+  </div>
+  </body>
+
+</template>
+
+<script setup lang="ts">
+</script>
+<style scoped>
+body{
+  margin: 0;
+  display: flex;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+.novel{
+  display: flex;
+  background-color: darkolivegreen;
+  height: 100%;
+  width: 100%;
+  justify-content: flex-end;
+  align-items: flex-end;
+}
+.game_text{
+  margin: 20px;
+  font-size: 25px;
+  color: white;
+}
+</style>
